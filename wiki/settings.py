c8baf8f35c2c4ababb59ccfa64815e4fa945b002
Piyush <piyushkumar16@protonmail.com>
2025-11-14 04:46:47 +0000

    Added force script name to load pages in code server

--- wiki/settings.py
+++ wiki/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# Running behind a proxy
+FORCE_SCRIPT_NAME = '/@ghost0seeker/cs50-web.main/apps/code-server/proxy/8000'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +28,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coder.ghostcastle.space',]
 
 
 # Application definition
